5c831cdf904808a2950dcbf616600a976858a6b0
Fernando Catalán <fernandoicatalan@gmail.com>
2024-01-15 19:53:18 -0300

    Updating config to deploy in Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
 });
